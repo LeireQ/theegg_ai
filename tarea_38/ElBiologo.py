3385a0dd53523757b23f5697b829bbf99fe11084
LeireQ <66866654+LeireQ@users.noreply.github.com>
2020-08-11 10:34:14 +0200

    Update ElBiologo.py

--- tarea_38/ElBiologo.py
+++ tarea_38/ElBiologo.py
@@ -1,4 +1,5 @@
-
+#función que comprueba que la variable de entrada 
+#solo contenga los caracteres ACGT
 def check_adn(text):
     text=text.upper()
     correct = "ACGT"
@@ -11,6 +12,7 @@ while 1:
     adn = (input("Introduzca primera secuencia de ADN: "), input("Introduzca segunda secuencia de ADN: "))
 
     if check_adn(adn[0])==True and check_adn(adn[1])==True:
+        #ordenar como secuencia más larga adn1
         if len(adn[0]) >= len(adn[1]) :
             adn1 = adn[0]
             adn2 = adn[1]
@@ -22,9 +24,15 @@ while 1:
         respuesta = ""
         aux_respuesta = ""
 
+        #recorrer el string adn1
         for i in range(len(adn1)):
+            #inicializar variables contadoy y respuesta
             cont = i
             respuesta = ""
+            #recorrer el string adn2 y compararlo con adn1
+            #si los valores coinciden, tomo la siguiente posición de ambos strings
+            #si no coinciden o termino de recorrer adn2, compruebo si el buffer respuesta
+            #tiene mayor longitud que auxiliar. aux_respuesta almacenará la respuesta final
             for j in range(len(adn2)):
                 if cont<len(adn1) and adn1[cont]==adn2[j]:
                     respuesta += adn1[cont]
@@ -36,7 +44,6 @@ while 1:
                     cont=i
             if(len(respuesta) > len(aux_respuesta)):
                 aux_respuesta = respuesta
-
 
         print("Bases adyacentes de mayor tamaño: "+aux_respuesta)
 
